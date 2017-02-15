d2e8484a2b14d067ef2ae7f72a1a1201d7d4e8ae
bearaxe <insetpun@gmail.com>
2017-02-14 21:50:17 -0500

    add Console

---
+++ Console.java
@@ -0,0 +1,45 @@
+//Does terminal monitoring.
+//This is a submanager that does input interpretation (for now)
+import java.util.Scanner;
+
+public class Console{
+
+  public void scanForInput(Printer printer){
+    Scanner input = new Scanner(System.in);
+    for (prompt(); input.hasNextLine(); prompt()){
+      String newMsg = input.nextLine();
+
+      if(newMsg.length() == 0)
+        continue;//skip next parts, go to next round
+
+      if(newMsg.trim().equalsIgnoreCase("exit")){
+        System.exit(0);
+      }
+
+      String[] args = newMsg.split("\\.");
+      readInput(args);
+      printer.boxPrint(args);
+    }
+  }
+
+  private void readInput(String[] args){
+    for(String argLine : args){
+      argLine = argLine.trim();
+      String keyword = argLine.split(" ")[0];
+      //this will eventually need to be a case statement.
+      if(keyword.equalsIgnoreCase("load")){
+        String fileLocation = argLine.substring(5);
+        loadFile(fileLocation);
+      }
+    }
+  }
+
+  private void loadFile(String fileLocation){
+    System.out.println("File at " + fileLocation + " loaded!");
+  }
+
+  private void prompt(){
+    System.out.print(">> ");
+  }
+
+}
