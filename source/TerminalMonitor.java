6da2a319206cd36c7549ff7eee26f52de9a782ab
bearaxe <insetpun@gmail.com>
2017-02-16 20:36:12 -0500

    cleaning

--- source/TerminalMonitor.java
+++ source/TerminalMonitor.java
@@ -1,5 +1,5 @@
 //Does terminal monitoring.
-//This does input interpretation.
+//This does not do input interpretation.
 //As much as I want to, I can't call it MonitorMonitor
 import java.util.Scanner;
 
@@ -19,37 +19,6 @@ public class TerminalMonitor{
       printer.boxPrint(args);
     }
   }
-
-  //this will get moved to what will be between terminalMonitor and webMonitor
-  //That would imply it belongs in DataController. And it does.
-  // private void readInput(String[] args){
-  //   for(String argLine : args){
-  //     argLine = argLine.trim();
-  //     String keyword = argLine.split(" ")[0];
-  //     int keySpaceLen = keyword.length()+1;
-  //
-  //     dataController.readCommand(keyword, keySpaceLen);
-  //
-  //     switch(keyword){
-  //       case "exit":
-  //         System.exit(0);
-  //         break;
-  //       case "load":
-  //         dataController.loadFile(argLine.substring(keySpaceLen));
-  //         break;
-  //       case "update":
-  //         dataController.updateQuery(argLine.substring(keySpaceLen), "Unimplemented");
-  //         break;
-  //       case "find":
-  //         dataController.getQuery(argLine.substring(keySpaceLen));
-  //         break;
-  //       case "save":
-  //         dataController.saveFile(argLine.substring(keySpaceLen));
-  //       default:
-  //         break; //do nothing, just print.
-  //       }
-  //     }
-  //   }
 
   private void prompt(){
     System.out.print(">> ");
